cf465794a0e2b4fd8285bff027ba211c259851a2
Angel Jimenez Reyes <angel.g.j.reyes@gmail.com>
2022-05-07 14:36:01 -0500

    Comit del proyecto

---
+++ AJIR-MCV-dotnetcore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AJIR_MCV_dotnetcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
