ad02763a767c7459da1bc30b09c68da0e5b37d1e
Angel Jimenez Reyes <angel.g.j.reyes@gmail.com>
2022-05-07 18:31:13 -0500

    MyLayout

--- AJIR-MCV-dotnetcore/Startup.cs
+++ AJIR-MCV-dotnetcore/Startup.cs
@@ -24,6 +24,7 @@ namespace AJIR_MCV_dotnetcore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddMvc().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +41,7 @@ namespace AJIR_MCV_dotnetcore
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
+            app.UseDefaultFiles();
             app.UseStaticFiles();
 
             app.UseRouting();
